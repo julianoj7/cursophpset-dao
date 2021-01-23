53da5b0dcfe885cc53c0cf998b2fd97824856fe5
Juliano Nogueira <jotaweb.art@gmail.com>
2021-01-23 14:43:18 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -2,10 +2,35 @@
 
 require_once("config.php");
 
-$root = new Usuario();
-$root->loadbyID(3);
+//$root = new Usuario();
+//$root->loadbyID(3);
 
-echo $root;
+//echo $root;
+
+
+
+//carrega uma lista de usuarios
+
+//$lista = Usuario::getList();
+
+//echo json_encode($lista);
+
+
+
+//carrega uma lista pelo login
+
+//$search = Usuario::search("jo");
+
+//echo json_encode($search);
+
+
+
+//carrega o usuario usando login e senha
+
+$usuario = new Usuario();
+$usuario->login("user","12345");
+
+echo $usuario;
 
 
 
